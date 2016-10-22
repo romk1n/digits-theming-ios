cec2ae9b81ac220f83d6700fa948ad89bd93aaad
Roman <roman@kamcord.com>
2016-10-21 17:22:02 -0700

    example of colors resulting in black text

--- DigitsThemesSwift/DigitsThemes/AppDelegate.swift
+++ DigitsThemesSwift/DigitsThemes/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        Fabric.with([Digits()])
+        Fabric.with([Digits.self])
         return true
     }
 
--- DigitsThemesSwift/DigitsThemes/ViewController.swift
+++ DigitsThemesSwift/DigitsThemes/ViewController.swift
@@ -12,34 +12,67 @@ import DigitsKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var btnLogin: UIButton!
+    @IBOutlet weak var btnLogin: UIButton! {
+        didSet {
+            btnLogin.hidden = true
+        }
+    }
+    
+    weak var authButton: DGTAuthenticateButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         Digits.sharedInstance().logOut()
+        
+        
+        self.createButton(UIColor.greenColor())
+        
+        // NONE of those colors work
+        let timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(update
+            ), userInfo: nil, repeats: true)
+
     }
     
-    @IBAction func didTapButton(sender: UIButton) {
-        let configuration = DGTAuthenticationConfiguration(accountFields: .DefaultOptionMask)
-        configuration.appearance = DGTAppearance()
-
-        configuration.appearance.logoImage = UIImage(named: "logo")
+    func update() {
         
-        configuration.appearance.labelFont = UIFont(name: "HelveticaNeue-Bold", size: 16)
-        configuration.appearance.bodyFont = UIFont(name: "HelveticaNeue-Italic", size: 16)
+        let customGreen = UIColor(red:79/255, green:195/255, blue:150/255, alpha:1)
+        let customBlue = UIColor(red:126/255, green:177/255, blue:230/255, alpha:1)
         
-        configuration.appearance.accentColor = UIColor(red:0.33, green:0.67, blue:0.93, alpha:1.0)
-        configuration.appearance.backgroundColor = UIColor(patternImage: UIImage(named: "bg-pattern")!)
+        let colors = [UIColor.greenColor(), customGreen, customBlue, UIColor.yellowColor()]
+        let index = Int(arc4random_uniform(UInt32(colors.count)))
+        self.authButton.removeFromSuperview()
+        self.createButton(colors[index])
+    }
+    
+    func createButton(color: UIColor) {
         
-        Digits.sharedInstance().authenticateWithViewController(self, configuration: configuration) { (session, error) -> Void in
+        let authButton = DGTAuthenticateButton(authenticationCompletion: { (session: DGTSession?, error: NSError?) in
             if (session != nil) {
-                self.btnLogin.setTitle("Your Digits User ID is " + session.userID, forState: UIControlState.Normal)
+                // TODO: associate the session userID with your user model
+                let message = "Phone number: \(session!.phoneNumber)"
+                let alertController = UIAlertController(title: "You are logged in!", message: message, preferredStyle: .Alert)
+                alertController.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: .None))
+                self.presentViewController(alertController, animated: true, completion: .None)
+            } else {
+                NSLog("Authentication error: %@", error!.localizedDescription)
             }
-            else {
-                print(error.localizedDescription)
-            }
+        })
+        self.authButton = authButton
+        self.authButton.center = self.view.center
+        self.authButton.digitsAppearance = {
             
-        }
+            let config = DGTAppearance()
+            config.backgroundColor = UIColor.whiteColor()
+            config.accentColor = color
+            
+            return config
+        }()
+        self.view.addSubview(self.authButton)
+    }
+
+    
+    @IBAction func didTapButton(sender: UIButton) {
+
    }
 }
